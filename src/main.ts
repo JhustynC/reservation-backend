0589c1a43d88621d826dabaa5c26d2e8e6b37cf1
JhustynC <christopherarias2002@gmail.com>
2025-02-21 19:57:21 -0500

    initial set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port: number = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? port);
+  console.log(`UP ON: http://localhost:${port}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => console.error(err));
